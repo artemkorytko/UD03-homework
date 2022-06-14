5e4578cc74d77fd122d6a357927915f8f8cbd3ba
Dmitry <haron90d@gmail.com>
2022-06-14 16:28:27 +0300

    Good

--- Assets/WhileLoop.cs
+++ Assets/WhileLoop.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class WhileLoop : MonoBehaviour
 {
+    int KillOrcs = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        while (KillOrcs > 0)
+        {
+            Debug.Log("i killed an orc");
+            KillOrcs--;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
