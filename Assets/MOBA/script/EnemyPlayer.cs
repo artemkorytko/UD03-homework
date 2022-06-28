773071937d48c57abaf772397abae0216c9953ba
Dmitry <haron90d@gmail.com>
2022-06-28 19:53:10 +0300

    1

--- Assets/MOBA/script/EnemyPlayer.cs
+++ Assets/MOBA/script/EnemyPlayer.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Moba
 {
     public class EnemyPlayer : AnimatedPlayer
     {
+        [SerializeField] protected GameObject _EnemyPlayer;
+        [SerializeField] protected float _distance;
+        NavMeshAgent nav;
+        public float Radius = 20;
+
+        private void Start()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+        private void Update()
+        {
+            _distance = Vector3.Distance(_EnemyPlayer.transform.position, transform.position);
+            gameObject.GetComponent<Animator>().SetTrigger("Idle");
+
+            if (_distance > Radius)
+            {
+                nav.enabled = false;
+            }
+            if (_distance > Radius)
+            {
+                nav.enabled = true;
+                nav.SetDestination(_EnemyPlayer.transform.position);
+                gameObject.GetComponent<Animator>().SetTrigger("Run");
+            }
+        }
+
 
     }
-
-
-
 }
--- Assets/MOBA/script/MovebelPlayer.cs
+++ Assets/MOBA/script/MovebelPlayer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 namespace Moba
 {
     public class MoveblePlayer : BasePlayer
