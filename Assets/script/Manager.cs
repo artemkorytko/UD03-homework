434c94549fbb7856b80b32d276f7d1c4a0d74d82
Dmitry <haron90d@gmail.com>
2022-07-25 17:55:32 +0300

    calculator 2
    script manager

--- Assets/script/Manager.cs
+++ Assets/script/Manager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
+    public VerticalLayoutGroup buttonGroup;
+    public HorizontalLayoutGroup buttomRow;
+    public RectTransform canvasRect;
+    Calculator2[] bottomButtons;
     // Start is called before the first frame update
     void Start()
     {
