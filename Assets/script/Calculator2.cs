232a94a195faeacf25721392c35d8007ff44a33d
Dmitry <haron90d@gmail.com>
2022-07-25 17:42:03 +0300

    Calculator2

---
+++ Assets/script/Calculator2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator2 : MonoBehaviour
+{
+    public Text label;
+    // Start is called before the first frame update)
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onTapped()
+    {
+        Debug.Log("Tapped: " + label.text);
+    }
+}
