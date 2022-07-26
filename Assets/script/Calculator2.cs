485231bb298a4ea32ec55593c5e7f6d8d34094a5
Dmitry <haron90d@gmail.com>
2022-07-26 15:18:28 +0300

    Calculator 2
     1

--- Assets/script/Calculator2.cs
+++ Assets/script/Calculator2.cs
@@ -5,20 +5,48 @@ using UnityEngine.UI;
 
 public class Calculator2 : MonoBehaviour
 {
-    public Text label;
-    // Start is called before the first frame update)
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void onTapped()
-    {
-        Debug.Log("Tapped: " + label.text);
-    }
+	public Text label;
+
+	public RectTransform rectTransform
+	{
+		get
+		{
+			if (_rectTransform == null)
+				_rectTransform = GetComponent<RectTransform>();
+			return _rectTransform;
+		}
+	}
+	RectTransform _rectTransform;
+
+	public Manager calcManager
+	{
+		get
+		{
+			if (_calcManager == null)
+				_calcManager = GetComponentInParent<Manager>();
+			return _calcManager;
+		}
+	}
+	static Manager _calcManager;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void onTapped()
+	{
+		Debug.Log("Tapped: " + label.text);
+		calcManager.buttonTapped(label.text[0]);
+	}
 }
+
